f2fa1ff281eb07453101fd453550d31a739d30b0
BT_2N2_14 <Sample@bths.edu>
2023-12-15 13:55:16 -0500

    max commit

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("i hate max!!!");
         System.out.println("Yo Tambien :)");
+        System.out.println("Quazi is poopy head");
     }
 }
